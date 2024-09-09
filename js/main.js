073313e9e1fe49aaafb466de537035b6d7c9d229
clara seijo <clasei@proton.me>
2024-09-09 17:16:58 +0200

    collision and count working, splice removing ok spaceships after crash

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const endScreenNode = document.querySelector("#end-screen")
 
 // buttons
 const startBtnNode = document.querySelector("#start-btn")
+const againBtnNode = document.querySelector("#play-again-btn")
 
 // game-box
 const gameBoxNode = document.querySelector("#game-box")
@@ -13,25 +14,27 @@ const gameBoxNode = document.querySelector("#game-box")
 // **********************************************************************
 // GLOBAL VARIABLES
 
-let hitchhikerObj = null; // hitchiker created and accesible
+let hitchhikerObj = null // hitchiker created and accesible
 
 let spaceshipsArray = []
-let spaceshipsFrequency = 700
+let spaceshipsFrequency = 500
 
-let gameIntervalId = null;
+let gameIntervalId = null
+let spaceshipsIntervalId = null
+
+let collisionCount = 0
 
 
 // **********************************************************************
 // GLOBAL FUNCTIONS
 
+
 // start game function
 function startGame() {
   // console.log('start click')
-
   // change start to game screen
   startScreenNode.style.display = "none"
   gameScreenNode.style.display = "flex"
-
   hitchhikerObj = new Hitchhiker() 
   // spaceshipObj = new Spaceship() // not needed, array created
 
@@ -45,22 +48,22 @@ function startGame() {
     moveSpaceship()
   }, spaceshipsFrequency)
 
-
 }
+
 
 function gameLoop() {
   // console.log('game loop starting')
-
   spaceshipsArray.forEach((eachSpaceship) => {
     eachSpaceship.moveDown()
   })
 
   detectSpaceshipColision()
+  removeSkippedSpaceships()
 
 }
 
-function moveSpaceship() {
 
+function moveSpaceship() {
   let randomPositionX = Math.random() * (gameBoxNode.offsetWidth - 37)
 
   let newSpaceship = new Spaceship(randomPositionX)
@@ -71,7 +74,7 @@ function moveSpaceship() {
 
 function detectSpaceshipColision() {
 
-  spaceshipsArray.forEach((eachSpaceship) => {
+  spaceshipsArray.forEach((eachSpaceship, index) => { // index added to remove each spaceship after collision
 
     if (
       hitchhikerObj.x < eachSpaceship.x + eachSpaceship.w &&
@@ -80,13 +83,43 @@ function detectSpaceshipColision() {
       hitchhikerObj.y + hitchhikerObj.h > eachSpaceship.y
     ) {
       console.log('hitchhiker crashed!')
-      gameOver()
+
+      collisionCount += 1
+      eachSpaceship.node.src = "../assets/explosion-0.png" // changes image to show
+
+      
+      eachSpaceship.node.remove()
+      spaceshipsArray.splice(index, 1)
+      
+      
+      
+      // collisionCount += 1
+
+      // setTimeout(() => {
+      //   eachSpaceship.node.remove()
+      //   spaceshipsArray.shift()
+      // }, 50)
+
+      if (collisionCount >= 3) {
+        gameOver()
+      }
     }
   })
 }
 
-function gameOver() {
 
+function removeSkippedSpaceships() {
+  spaceshipsArray.forEach((eachSpaceship, index) => { // index used to remove each element of the array
+    if (eachSpaceship.y > gameBoxNode.offsetHeight) {
+      eachSpaceship.node.remove() // removes spaceship from the DOM
+      spaceshipsArray.splice(index, 1) // removes spaceship from the array
+      console.log('spaceship removed')
+    }
+  })
+}
+
+
+function gameOver() {
   clearInterval(gameIntervalId)
   clearInterval(spaceshipsIntervalId)
 
@@ -94,6 +127,13 @@ function gameOver() {
   endScreenNode.style.display = "flex"
 }
 
+// function resetGame() {
+//   spaceshipsArray.forEach((eachSpaceship) => {
+//     eachSpaceship.node.remove()
+//   })
+//   spaceshipsArray = []
+//   hitchhikerObj = null
+// }
 
 
 // **********************************************************************
@@ -101,6 +141,8 @@ function gameOver() {
 
 // start button click
 startBtnNode.addEventListener("click", startGame)
+
+// againBtnNode.addEventListener("click", startGame)
 
 document.addEventListener("keydown", (event) => {
   // console.log('pressing key')
